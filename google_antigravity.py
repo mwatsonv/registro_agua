ddc1bc4e88d1a1952852a6dde974f287338fca8c
Miguel <mwatsonv@gmail.com>
2026-07-26 15:15:21 -0500

    Fix: resolucion segura de corrutina/objeto en structured_output

--- google_antigravity.py
+++ google_antigravity.py
@@ -20,8 +20,8 @@ class ChatResponseWrapper:
         return CallableString(val)
 
     @property
-    def structured_output(self):
-        return self._real_response.structured_output()
+def structured_output(self):
+    return getattr(self._real_response, "structured_output", None)
 
     def __getattr__(self, name):
         return getattr(self._real_response, name)
